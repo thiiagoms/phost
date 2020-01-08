629590def4ebd35046ace0a2f0d95e6db9debbda
thiago.martins <thiago.martins@getrak.com.br>
2020-01-08 13:54:14 -0300

    :baloon: Old version

---
+++ phost.old.php
@@ -0,0 +1,47 @@
+<?php
+
+  print("[*] PHost Tool\n");
+  $webpath = "/var/www/html";
+  $vhost = "testng";
+  $extension = "tk";
+
+  try {
+    $user = get_current_user();
+    $complete_path = "{$webpath}/{$vhost}";
+    $mkdir = "mkdir -p {$complete_path}";
+    $brackets = array("{", "}");
+    print("[*] Creating dir...\n");
+    shell_exec($mkdir);
+    print("[*] Change group...\n");
+    $group = "chown -R {$user}:{$user} $complete_path";
+    shell_exec($group);
+    print("[*] Right permissions...\n");
+    $permissions = "chmod -R 755 {$webpath}";
+    shell_exec($permissions);
+    print("[*] Creating apache2 config file...\n");
+    $apache2_config_file = <<<EOT
+    "<VirtualHost *:80>
+        ServerAdmin {$user}@admin.{$extension}
+        ServerName $vhost.$extension
+        ServerAlias www.$vhost.$extension
+        DocumentRoot  $complete_path
+    </VirtualHost>"
+EOT;
+    print("[*] Copy to default dir...\n");
+    shell_exec("echo {$apache2_config_file} > {$vhost}.{$extension}.conf");
+    $copy_file = "cp {$vhost}.{$extension}.conf /etc/apache2/sites-available/{$vhost}.{$extension}.conf";
+    shell_exec($copy_file);
+    print("[*] Restarting apache2...\n");
+    shell_exec("systemctl restart apache2.service");
+    print("[*] Writing on hosts file....\n");
+    $hosts  = "127.0.0.1  {$vhost}.{$extension}";
+    shell_exec("echo {$hosts} >> /etc/hosts");
+    print("[*] Activate vhost config");
+    shell_exec("a2ensite {$vhost}.{$extension}.conf");
+    shell_exec("systemctl restart apache2.service");
+    print("[*] No error reporting, thanks to use it! :D");
+  } catch(Exception $e) {
+    print("Error: {$e->getMessage()}");
+  }
+
+?>
